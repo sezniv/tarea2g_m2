e99611e4cbecaef49ad0ce1692b6140a6a11a38f
Maguilera85 <drianketing@gmail.com>
2020-10-30 12:40:21 -0300

    se termina flotantes, trabajo completo, falta revisar

--- float.py
+++ float.py
@@ -12,7 +12,7 @@ for n in range(0,5):
         string_aleatorio += random.choice(caracteres)    
         flotante = "{0:.3f}".format(float(string_aleatorio) / 3) 
     lista.append(flotante)
-print(lista)
+print("Esta es la lista original random " + str(lista))
 
 # aca estoy sumando cada numero que contiene cada un elemento dentro de la lista
 
@@ -22,10 +22,16 @@ for elemento in lista:
     cont3 = cont1 + cont2
     cont1 = cont3
 resultado = cont1 / 5
-resultadox = "{0:.3f}".format(resultado)
-print("el promedio de los valores dentro de la lista es de: " + str(resultadox))
+promedio = "{0:.3f}".format(resultado)
+print("el promedio de los valores dentro de la lista random es de: " + str(promedio))
 
+
+maximo = max(lista)
 for elemento in lista:
-    resultado1 = float(elemento) - resultadox
-for elemento in lista:
-    ele1 = float(elemento)
+    resultado1 = float(elemento) - float(promedio)
+    n_lista = resultado1 / float(maximo)
+    var_x = 0
+    contenedor = n_lista
+    var_x += contenedor
+print("esta el la suma total de cada elemento de la lista modificada: ")
+print(var_x)
